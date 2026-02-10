7dacf8b1a02518c0503a7149b6a35b8515fbe060
Santylx <102611029+Santylx@users.noreply.github.com>
2026-02-10 15:02:09 -0500

    style: remove layout top padding to eliminate white band

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
       >
         <Navigation />
-        <main className="flex-grow pt-20">
+        <main className="flex-grow">
           {children}
         </main>
         <Footer />
